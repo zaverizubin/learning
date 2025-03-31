6e2cc2f3a080ae5c3a9b648196ba1cf85e309196
ZUBIN-PC\zaver <zaverizubin@gmail.com>
2025-03-31 12:23:13 +0530

    Breaking up code into separate projects

---
+++ java/liskov_principle/src/main/java/com/playground/Main.java
@@ -0,0 +1,17 @@
+package com.playground;
+
+import com.playground.data_structures.trees.NestedSets;
+
+public class Main {
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		NestedSets nestedSets = new NestedSets();
+		nestedSets.buildTree();
+		nestedSets.node1.toString();
+	}
+
+	public Main() {
+		super();
+	}
+
+}
